b3272970a1dd8fa2881665d081bcb8dd3676da80
Jonathan Whittle <jonathan.m.whittle@gmail.com>
2018-11-01 17:08:23 -0400

    heroku build

---
+++ build/precache-manifest.a41dd6bffa1599e747283a3aba90569e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "12bccf0603ec909ebb5f5488696f6e3f",
+    "url": "/static/media/flogo_RGB_HEX-144.12bccf06.svg"
+  },
+  {
+    "revision": "c68df5c5585c900dae22",
+    "url": "/static/css/main.fef689ee.chunk.css"
+  },
+  {
+    "revision": "bbb63d50e81ff2e9974b",
+    "url": "/static/js/1.bbb63d50.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1ed2ccd8fea73171b5e3f512605cd627",
+    "url": "/static/media/front-wedding.1ed2ccd8.jpg"
+  },
+  {
+    "revision": "c68df5c5585c900dae22",
+    "url": "/static/js/main.c68df5c5.chunk.js"
+  },
+  {
+    "revision": "a7a925c7c07f163f3274df0c1ab2c321",
+    "url": "/static/media/Twitter_Logo_Blue.a7a925c7.svg"
+  },
+  {
+    "revision": "0e84d5926187e7a3b785febd3a55bf1d",
+    "url": "/static/media/glyph-logo_May2016.0e84d592.png"
+  },
+  {
+    "revision": "c5d1356a3122e1970afb787cf942d404",
+    "url": "/static/media/logo.c5d1356a.svg"
+  },
+  {
+    "revision": "04eb830ef92fb140a5b8f2b781694f94",
+    "url": "/static/media/coding_background.04eb830e.jpg"
+  },
+  {
+    "revision": "2a74d252876b24c26b35dec2cfcada72",
+    "url": "/index.html"
+  }
+];
