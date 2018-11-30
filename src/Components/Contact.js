652a1b2b6f2d3ff8600a756a0145b2d392b3d752
Jonathan Whittle <jonathan.m.whittle@gmail.com>
2018-11-30 11:02:06 -0500

    create inline svg for Linkedin, Instagram, remove img tags

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react'
-// import facebook from '../img/facebook.svg'
-// import twitter from '../img/twitter.svg'
-import instagram from '../img/instagram.png'
-import linkedin from '../img/linkedin.png'
-// import gmail from  '../img/gmail.svg'
 
 import Facebook from './SvgComponents/facebook'
 import Gmail from './SvgComponents/gmail'
 import Twitter from './SvgComponents/twitter'
+import Instagram from './SvgComponents/instagram'
+import Linkedin from './SvgComponents/linkedin'
 
 class Contact extends Component {
 
@@ -17,19 +14,19 @@ class Contact extends Component {
         <h2>Contact</h2>
           <p>Feel free to contact me through any of the following means.</p>
         <div className="logo-flex">
-          <a className="social-background" href="https://www.facebook.com/jonathan.whittle.39" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.facebook.com/jonathan.whittle.39" target="_blank" rel="noopener noreferrer">
             <Facebook />
           </a>
-          <a className="social-background" href="https://twitter.com/JonathanWhittle" target="_blank" rel="noopener noreferrer">
+          <a href="https://twitter.com/JonathanWhittle" target="_blank" rel="noopener noreferrer">
             <Twitter />
           </a>
-          <a className="social-background" href="http://www.linkedin.com/in/jonathan-whittle-161590169" target="_blank" rel="noopener noreferrer">
-            <img src={ linkedin } className="linkedin logo-link" alt="logo"/>
+          <a href="http://www.linkedin.com/in/jonathan-whittle-161590169" target="_blank" rel="noopener noreferrer">
+            <Linkedin />
           </a>
-          <a className="social-background" href="https://www.instagram.com/jwhittl/" target="_blank" rel="noopener noreferrer">
-            <img src={ instagram } className="instagram logo-link" alt="logo"/>
+          <a href="https://www.instagram.com/jwhittl/" target="_blank" rel="noopener noreferrer">
+            <Instagram />
           </a>
-          <a className="social-background" href="mailto:jonathan.m.whittle@gmail.com" rel="noopener noreferrer">
+          <a href="mailto:jonathan.m.whittle@gmail.com" rel="noopener noreferrer">
             <Gmail />
           </a>
         </div>
